0e0e4b73973f9ab8179b13cb23c5338950cbe84b
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-06-28 14:59:22 +0200

    fix: make setup routine work

--- push-game.js
+++ push-game.js
@@ -5,27 +5,45 @@ const pixel = require('node-pixel');
 const DIGITAL_MODE = 6;
 const STRIP_PIN = 6;
 
-const STATE = {
+const setup = {
 	ORIGIN_PIN: null,
 	MAX_X_PIN: null,
 	MAX_Y_PIN: null
 };
 
-async function game() {
-	const board = await createBoard();
-	console.log(STATE);
-	STATE.ORIGIN_PIN = await getOrigin(board);
-	console.log(STATE);
-	STATE.MAX_X_PIN = await getPinPress(board);
-	console.log(STATE);
-	STATE.MAX_Y_PIN = await getPinPress(board);
-	console.log(STATE);
+async function game(config = {}) {
+	const setup = config.setup || await getSetup();
 }
 
 game()
 	.catch(err => {
 		throw err;
 	});
+
+async function getSetup() {
+	const s = {};
+
+	const board = await createBoard();
+	s.ORIGIN_PIN = await getOrigin(board);
+
+	const strip = await createStrip(board);
+
+	strip.pixel(0).color('green');
+	strip.show();
+
+	s.MAX_X_PIN = await getPinPress(board);
+	strip.pixel(1).color('green');
+	strip.show();
+
+	s.MAX_Y_PIN = await getPinPress(board);
+	strip.pixel(2).color('green');
+	strip.show();
+
+	strip.color('green');
+	strip.show();
+
+	return s;
+}
 
 function getPinPress(board, opts = {}) {
 	const {initial = false} = opts;
@@ -38,33 +56,30 @@ function getPinPress(board, opts = {}) {
 			.filter(pin => pin !== STRIP_PIN)
 			.map(createButtonFromPin);
 
-		let initialized = [];
-
 		// Ignore bogus first down event from all buttons
-		testButtons.forEach(testButton => {
-			testButton.on('down', () => {
-				console.log(testButton.pin);
-				if (initial === false || initialized.includes(testButton.pin)) {
-					return resolve(testButton.pin);
-				}
-				initialized.push(testButton.pin);
-			});
-		});
+		setTimeout(() =>{
+			testButtons.forEach(testButton => {
+				const pin = testButton.pin;
 
-		// Assume all buttons are initialized/do not fire bogus initial down events after one second
-		if (initial) {
-			setTimeout(() => {
-				initialized = testButtons.map(testButton => testButton.pin);
-			}, 1000);
-		}
+				// Ignore bogus down events for pins 0 and 1
+				if (pin === 0 || pin === 1) {
+					return;
+				}
+
+				function onDown() {
+					testButtons.forEach(testButton => testButton.removeAllListeners('down', onDown));
+					resolve(pin);
+				}
+
+				testButton.on('down', onDown);
+			});
+		}, 500);
 	});
 }
 
 async function getOrigin(board) {
-	const maxPin = Object.keys(board.pins).length;
-
 	// Initiate test strip
-	const testStrip = await createStripWithLength(board, maxPin);
+	const testStrip = await createStrip(board);
 
 	// prompt user input
 	testStrip.pixel(0).color('red');
@@ -86,7 +101,9 @@ function createButtonFromPin(pin) {
 	return new five.Button({pin, isPullup: true});
 }
 
-function createStripWithLength(board, length) {
+function createStrip(board, opts = {}) {
+	const {length = Object.keys(board.pins).length} = opts;
+
 	return new Promise((resolve, reject) => {
 		const strip = new pixel.Strip({
 			board,
--- readme.md
+++ readme.md
@@ -7,29 +7,16 @@
 - Arcade Buttons
 
 ## Getting started
-
 - Connect the Arduino to the appropriate pins on a the LED breadboard
-- Power up the Arduino
-- Perform steps for Local Development Environment if necessary
-
-## Local Development Environment
-- [Arduino IDE 1.8.3](https://www.arduino.cc/en/Main/Software)
-- NodeJS `>= 6`
-
-### Arduino development
-- open Arduino IDE
-- under Sketch -> Include Library -> Manage Libraries -> Install Firmata
-- under Tools -> select Arduino/Genuino Mega or Mega 2560
-- under Tools -> select Port /dev/usbmodem[xxxx]
-- to verify the connection click under Tools -> Get Board Info
-- open node_pixel_firmata/node_pixel_firmata.ino
-- upload code to arduino over Sketch -> Upload
-
-### Node Johnny Five app
 
 ```
-node push-game.js
+n 8.1.2
+npm install
+npm start
 ```
+
+## Prerequesites
+- NodeJS `>= 8`
 
 ## Architecture
 
