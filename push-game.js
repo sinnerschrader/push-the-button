a4412f8d7efe720e63cb354e7709f5bd2f086a65
Martin Fieber <martin.fieber@sinnerschrader.com>
2017-06-28 14:20:17 +0200

    feat: start routine setup

--- push-game.js
+++ push-game.js
@@ -1,85 +1,157 @@
-let five = require("johnny-five");
-let board = new five.Board();
-let pixel = require("node-pixel");
+const five = require('johnny-five');
+const pixel = require('node-pixel');
 
-let strip = null;
+// Assuming this means "digital" in board.pins
+const DIGITAL_MODE = 6;
+const STRIP_PIN = 6;
 
-board.on("ready", function() {
+const STATE = {
+	ORIGIN_PIN: null,
+	MAX_X_PIN: null,
+	MAX_Y_PIN: null
+};
 
-  console.log('BOARD is ready');
+async function game() {
+	const board = await createBoard();
+	console.log(STATE);
+	STATE.ORIGIN_PIN = await getOrigin(board);
+	console.log(STATE);
+	STATE.MAX_X_PIN = await getPinPress(board);
+	console.log(STATE);
+	STATE.MAX_Y_PIN = await getPinPress(board);
+	console.log(STATE);
+}
 
-  const button1 = new five.Button({
-    pin: 51,
-    isPullup: true
-  });
+game()
+	.catch(err => {
+		throw err;
+	});
 
-  const button2 = new five.Button({
-    pin: 53,
-    isPullup: true
-  });
+function getPinPress(board, opts = {}) {
+	const {initial = false} = opts;
 
-  strip = new pixel.Strip({
-    board: this,
-    controller: 'FIRMATA',
-    data: 13,
-    length: 4,
-    color_order: pixel.COLOR_ORDER.RGB
-  });
+	return new Promise(async (resolve, reject) => {
+		const maxPin = Object.keys(board.pins).length;
 
-  strip.on("ready", function() {
-    console.log("Strip ready, let's go");
-  });
+		// Initiate test buttons
+		const testButtons = range(0, maxPin)
+			.filter(pin => pin !== STRIP_PIN)
+			.map(createButtonFromPin);
 
-  button1.on("down", function() {
-    staticRainbow();
-  });
+		let initialized = [];
 
-  button1.on("up", function() {
-    strip.off();
-  });
+		// Ignore bogus first down event from all buttons
+		testButtons.forEach(testButton => {
+			testButton.on('down', () => {
+				console.log(testButton.pin);
+				if (initial === false || initialized.includes(testButton.pin)) {
+					return resolve(testButton.pin);
+				}
+				initialized.push(testButton.pin);
+			});
+		});
 
-  button2.on("down", function() {
-    strip.colour("orange");
-    strip.show();
-  });
+		// Assume all buttons are initialized/do not fire bogus initial down events after one second
+		if (initial) {
+			setTimeout(() => {
+				initialized = testButtons.map(testButton => testButton.pin);
+			}, 1000);
+		}
+	});
+}
 
-  button2.on("up", function() {
-    strip.off();
-  });
+async function getOrigin(board) {
+	const maxPin = Object.keys(board.pins).length;
 
-  function staticRainbow(){
-    console.log('staticRainbow');
+	// Initiate test strip
+	const testStrip = await createStripWithLength(board, maxPin);
 
-    var showColor;
-    for(var i = 0; i < strip.length; i++) {
-        showColor = colorWheel( ( i*256 / strip.length ) & 255 );
-        strip.pixel( i ).color( showColor );
-    }
-    strip.show();
-  }
+	// prompt user input
+	testStrip.pixel(0).color('red');
+	testStrip.show();
 
-  // Input a value 0 to 255 to get a color value.
-  // The colours are a transition r - g - b - back to r.
-  function colorWheel( WheelPos ){
-    var r,g,b;
-    WheelPos = 255 - WheelPos;
+	// Wait for user to press the correct button
+	return getPinPress(board, {initial: true});
+}
 
-    if ( WheelPos < 85 ) {
-        r = 255 - WheelPos * 3;
-        g = 0;
-        b = WheelPos * 3;
-    } else if (WheelPos < 170) {
-        WheelPos -= 85;
-        r = 0;
-        g = WheelPos * 3;
-        b = 255 - WheelPos * 3;
-    } else {
-        WheelPos -= 170;
-        r = WheelPos * 3;
-        g = 255 - WheelPos * 3;
-        b = 0;
-    }
-    // returns a string with the rgb value to be used as the parameter
-    return "rgb(" + r +"," + g + "," + b + ")";
-  }
-});
+function createBoard() {
+	return new Promise((resolve, reject) => {
+		const board = new five.Board();
+		board.on('ready', () => resolve(board));
+		board.on('error', reject);
+	});
+}
+
+function createButtonFromPin(pin) {
+	return new five.Button({pin, isPullup: true});
+}
+
+function createStripWithLength(board, length) {
+	return new Promise((resolve, reject) => {
+		const strip = new pixel.Strip({
+			board,
+			controller: 'FIRMATA',
+			data: STRIP_PIN,
+			length,
+			color_order: pixel.COLOR_ORDER.RGB
+		});
+		strip.on('ready', () => resolve(strip));
+	});
+}
+
+function range(start, count) {
+	return new Array(count)
+		.fill(true)
+		.map((_, i) => start + i);
+}
+
+/* board.on('ready', function () {
+
+	console.log('BOARD is ready');
+
+	const isPullup = true;
+
+	const button1 = new five.Button({ pin: 50, isPullup });
+	const button2 = new five.Button({ pin: 51, isPullup });
+	const button3 = new five.Button({ pin: 52, isPullup });
+	const button4 = new five.Button({ pin: 53, isPullup });
+
+	const strip = new pixel.Strip({
+		board: this,
+		controller: 'FIRMATA',
+		data: 6,
+		length: 4,
+		color_order: pixel.COLOR_ORDER.RGB
+	});
+
+	strip.on('ready', () => {
+		console.log('READY!');
+		strip.color('blue');
+		strip.show();
+	});
+
+	button1.on('up', () => strip.off());
+	button2.on('up', () => strip.off());
+	button3.on('up', () => strip.off());
+	button4.on('up', () => strip.off());
+
+	button1.on('down', () => {
+		strip.pixel(0).color('red');
+		strip.show();
+	});
+
+	button2.on('down', () => {
+		strip.pixel(1).color('orange');
+		strip.show();
+	});
+
+	button3.on('down', () => {
+		strip.pixel(2).color('green');
+		strip.show();
+	});
+
+	button4.on('down', () => {
+		strip.pixel(3).color('blue');
+		strip.show();
+	});
+}); */
